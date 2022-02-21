0802dd0e19c0e41da1f0b756d4441e47589d8166
jake <loncerasjakerei@gmail.com>
2022-02-22 07:31:34 +0800

    Clean up utils

--- src/res/components/global/utils.js
+++ src/res/components/global/utils.js
@@ -3,7 +3,6 @@ import { SignificantValue } from "./constants";
 export const currencyFormat = (value) => {
   const floatValue  = parseFloat(value);
   const { TWO }     = SignificantValue;
-  let rounded       = Math.round(floatValue * TWO) / TWO;
-  return rounded;
+  return Math.round(floatValue * TWO) / TWO;
 };
 
