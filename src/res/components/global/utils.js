f88c5868e9e665e8b0b5acf9aa81854344a324b5
jake <loncerasjakerei@gmail.com>
2022-02-22 10:27:23 +0800

    Clean up whitespaces

--- src/res/components/global/utils.js
+++ src/res/components/global/utils.js
@@ -2,7 +2,7 @@ import { SignificantValue } from "./constants";
 
 export const currencyFormat = (value) => {
   const floatValue  = parseFloat(value);
-  const { TWO }     = SignificantValue;
+  const {TWO}       = SignificantValue;
   return Math.round(floatValue * TWO) / TWO;
 };
 
