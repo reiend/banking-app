f1e20d95f2699d9cec9420d65bd987394036e708
jake <loncerasjakerei@gmail.com>
2022-02-23 21:32:17 +0800

    Fix File structure for expenses

---
+++ src/res/components/expenses/ExpenseItem.jsx
@@ -0,0 +1,29 @@
+import { PropTypes }         from "../proptypes/proptypes";
+import { AccountOption}      from "../global/constants";
+import { useAccountContext } from "../context/AccountContext";  
+
+const processExpenseItemDelete = (id, setExpenses) => {
+  const {ExpensesOption} = AccountOption;
+  const {DELETE_EXPENSE} = ExpensesOption;
+  
+  // process deleting expense
+  setExpenses({type: DELETE_EXPENSE, id})
+};
+
+export const ExpenseItem = ({name, value, id}) => {
+  const {setAccount}       = useAccountContext();
+
+  const onClickItemDelete  = () => processExpenseItemDelete(id, setAccount);
+
+  return( 
+    <li id={name}>
+      <span>{name}</span>-<span>{value}</span>
+      <button onClick={onClickItemDelete}>X</button>
+      <button>edit</button>
+    </li>
+  );
+};
+
+const {ExpenseItemProptypes} = PropTypes;
+ExpenseItem.propTypes        = {...ExpenseItemProptypes}
+
---
+++ src/res/components/expenses/ExpensesList.jsx
@@ -0,0 +1,32 @@
+import { ExpenseItem }       from "./ExpenseItem";
+import { Random }            from "../global/utils";
+import { useExpensesList }   from "../states/ExpensesListState";
+import { useAccountContext } from "../context/AccountContext";  
+import { useEffect }         from "react";
+
+const processExpensesList = (account, setExpensesList) => {
+  return () => setExpensesList(account.expenses.map((expense, i)=> {
+    const [name]          = Object.keys(expense);
+    const [value]         = Object.values(expense);
+    const expenseItemKey  = Random.getKey(name);
+    const expenseItemInfo = { name: `${name}`, value: `${value}`, }
+
+    return <ExpenseItem key={expenseItemKey} {...expenseItemInfo} id={i}/>;
+  }));
+};
+
+export const ExpensesList = () => {
+  const [expensesList, setExpensesList] = useExpensesList();
+  const {account}                       = useAccountContext();
+  const expensesLength                  = account.expenses.length;
+
+  useEffect(processExpensesList(account, setExpensesList), [expensesLength]);
+
+  return(
+    <div>
+      <h4>List of Expenses</h4>
+      <ul>{expensesList}</ul>
+    </div>
+  );
+};
+
