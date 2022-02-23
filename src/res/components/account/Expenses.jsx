719714fdce04607ebad457cc83c28dbe442af8df
jake <loncerasjakerei@gmail.com>
2022-02-23 14:57:27 +0800

    💡 Display Account information

--- src/res/components/account/Expenses.jsx
+++ src/res/components/account/Expenses.jsx
@@ -4,6 +4,7 @@ import { ExpensesList }        from "../coreutils/ExpensesUtils";
 export const Expenses = () => {
   return(
     <section>
+      <h3>Expenses</h3>
       <ExpensesList />
       <ExpenseInput/>  
     </section>  
