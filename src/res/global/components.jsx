bf78bc77bf45acfef973c1683f04f4e15dbeaabb
jake <loncerasjakerei@gmail.com>
2022-02-26 00:29:16 +0800

    🧬🔳 Make Expense button generic

--- src/res/global/components.jsx
+++ src/res/global/components.jsx
@@ -1,11 +1,19 @@
-import Proptype       from "prop-types";
+import { PropTypes } from "res/proptypes/proptypes";
 import { forwardRef } from "react";
 
-export const Input = forwardRef(({name}, ref) => <input 
+const Input = forwardRef(({name}, ref) => <input 
   type="text" 
   id={name} 
   name={name} 
   ref={ref} />);
 
-Input.propTypes    = {name: Proptype.string};
+const Button = ({title, onClick, name}) => {
+  if(!title) return <button name={name} onClick={onClick}>{name}</button>
+  if(!name)  return <button onClick={onClick}>{title}</button>
+};
 
+const {InputPropTypes, ButtonPropTypes} = PropTypes;
+Button.propTypes                        = {...ButtonPropTypes};
+Input.propTypes                         = {...InputPropTypes};
+
+export {Input, Button};
