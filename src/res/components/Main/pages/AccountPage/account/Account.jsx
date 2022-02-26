0d6b313c32c0dbcd8c8113a696ee4189548d30d1
jake <loncerasjakerei@gmail.com>
2022-02-26 11:42:18 +0800

    🧹 Clean ups on Account component

--- src/res/components/Main/pages/AccountPage/account/Account.jsx
+++ src/res/components/Main/pages/AccountPage/account/Account.jsx
@@ -1,33 +1,14 @@
-import { AccountOption, ExpenseInfo } from "res/global/constants";
-
 import { useAccountContext }  from "res/context/AccountContext";  
-import { useEffect }          from "react";
 
 export const Account = () => {
-  const {ExpensesOption}       = AccountOption;
-  const {UPDATE_TOTAL_EXPENSE} = ExpensesOption;
-  const {EXPENSE_VALUE}        = ExpenseInfo;
-
-  const {account, setAccount: setExpenses} = useAccountContext();
-
-  const {
-    isEdited, 
+  const {account} = useAccountContext();
+  const { 
     id, 
     firstname, 
     lastname, 
-    balance,
-    totalExpenses,
+    balance, 
+    totalExpenses, 
   } = account;
-  
-  // Update Total Expenses display
-  // useEffect(() => {
-  //   if(!account.expenses.length) return;
-  //   const totalExpenses = account
-  //     .expenses
-  //       .map((expense) => Object.values(expense)[EXPENSE_VALUE])
-  //       .reduce((totalExpenses, nextExpense) => totalExpenses + nextExpense);
-  //   setExpenses({type: UPDATE_TOTAL_EXPENSE, totalExpenses});
-  // }, [isEdited]);
 
   return(
     <section>
